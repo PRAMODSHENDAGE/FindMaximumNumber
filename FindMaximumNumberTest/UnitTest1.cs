ff7296bde834621e55c0fbeb48d583fbe7de8635
KajalShendage <shendagekajal123@gmail.com>
2022-09-14 00:13:15 +0530

    [Pramod] Add: Maximum Float Number

--- FindMaximumNumberTest/UnitTest1.cs
+++ FindMaximumNumberTest/UnitTest1.cs
@@ -32,5 +32,29 @@ namespace FindMaximumNumberTest
             int num = max.FindMaxIntNumber(1, 2, 3);
             Assert.AreEqual(num, 3);
         }
+        //TC 2.1
+        [Test]
+        public void GivenMaxFirsFloatNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            float num = max.FindMaxFloatNumber(3.1f, 1.2f, 2.4f);
+            Assert.AreEqual(num, 3.1f);
+        }
+        //TC 2.2
+        [Test]
+        public void GivenMaxSecondFloatNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            float num = max.FindMaxFloatNumber(1.2f, 3.1f, 2.4f);
+            Assert.AreEqual(num, 3.1f);
+        }
+        //TC 2.3
+        [Test]
+        public void GivenMaxThirdFloatNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            float num = max.FindMaxFloatNumber(1.2f, 2.4f, 3.1f);
+            Assert.AreEqual(num, 3.1f);
+        }
     }
 }
